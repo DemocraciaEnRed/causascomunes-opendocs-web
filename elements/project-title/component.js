c90b8f002ec3e1513e770c3f5bc481ab37cd881c
Wencha <wencha_@hotmail.com>
2019-08-05 14:54:25 -0300

    title de proyectos cambiado

--- elements/project-title/component.js
+++ elements/project-title/component.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const ProjectTitle = styled.h1`
-  font-size: 6rem;
+  font-size: 4rem;
   font-family: var(--bold);
   line-height: 1.08;
-  color: #203340;
+  color: var(--color-azul-principal);
   align-self: flex-end;
   @media(max-width:700px){
     align-self: flex-start;
   }
   padding-top:4rem;
+  text-transform: uppercase;
 `
 ProjectTitle.propTypes = {
   children: PropTypes.string.isRequired
