3f032428ee53eb4e4842989531a32ff7e8dadc8f
Wencha <wencha_@hotmail.com>
2019-08-22 11:15:54 -0300

    remove fav icons

--- pages/_document.js
+++ pages/_document.js
@@ -175,10 +175,6 @@ export default class MyDocument extends Document {
             NODE_ENV === 'production' &&
             <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-109170776-3');` }} />
           }
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon.ico' /> 
         </Head>
         <body>
           <Main />
