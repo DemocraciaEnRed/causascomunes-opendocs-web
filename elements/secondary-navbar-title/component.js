ae5801dd02df83195818f6f2a110ffe14db6c727
wencha <wencha_@hotmail.com>
2019-09-16 16:29:19 -0300

    link a home en nav secondary (portal propuestas)

--- elements/secondary-navbar-title/component.js
+++ elements/secondary-navbar-title/component.js
@@ -15,7 +15,7 @@ const Span = styled.span`
 
 const SecondaryNavbarTitle = () => (
   <StyledSecondaryNavbarTitle>
-   Portal de <Span>Propuestas</Span>
+   <a href="/">Portal de <Span>Propuestas</Span></a>
   </StyledSecondaryNavbarTitle>
 )
 
