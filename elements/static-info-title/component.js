a8f8d31e4444facf95513edb52deac53234a16d0
wencha <wencha_@hotmail.com>
2019-09-20 12:50:26 -0300

    fix line height de títulos de acerca de (faq, terminos y conds, etc.)

--- elements/static-info-title/component.js
+++ elements/static-info-title/component.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components'
 
 const StaticInfoTitle = styled.h1`
+  font-family: var(--bold);
   font-size: 5.5rem;
+  color: #203340;
+  margin-top:23px;
+  line-height: 6.18rem;
+  margin-bottom: 50px;
   @media(max-width:800px){
     line-height:7rem;
     margin-bottom:30px;
   }
-  font-family: var(--bold);
-  line-height: 1.18rem;
-  color: #203340;
-  margin-top:23px;
-  margin-bottom: 80px;
 `
 
 export default StaticInfoTitle
